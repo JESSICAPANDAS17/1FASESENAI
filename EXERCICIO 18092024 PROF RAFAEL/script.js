938f4e2017943961e719407edd9e8af93478b545
JESSICA ALMEIDA <jessica.pandas17@gmail.com>
2024-09-18 22:30:08 -0300

    Exercícios de Breecrowd

---
+++ EXERCICIO 18092024 PROF RAFAEL/script.js
@@ -0,0 +1,19 @@
+function Executar1008(){
+    let id, horas, valorHora, salario
+    //let horas
+    //let valorHora
+    //let salario
+    id = Number(prompt("Digite seu id:"))
+    //id = Number('45')
+    //id = 45
+    horas = Number(prompt("Quantas horas você trabalhou?"))
+    valorHora = Number(prompt("Quantos você ganha por hora?"))
+
+    salario = horas * valorHora
+
+    alert("ID: " + "\nSalário: R$" + salario.toFixed(2))
+}
+
+function Executar1009(){
+    let
+}
